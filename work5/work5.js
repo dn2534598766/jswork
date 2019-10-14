c666d3cacab82f4fc430506f27d379013d1918d7
dn2534598766 <2534598766@qq.com>
2019-10-14 11:11:47 +0800

    work5

--- work5/work5.js
+++ work5/work5.js
@@ -8,3 +8,26 @@ for(let i=9;i>0;--i){
 }
 str+='</table>'
 document.getElementById('table2').innerHTML=str
+
+str2 ='<table border="1">'
+for(let i=9;i>0;--i){
+    str2+='<tr>'
+    for(var j=9;j>0;--j){
+        str2+='<td>&nbsp;</td>'
+    }
+    str2+='</tr>'
+}
+str2+='</table>'
+document.getElementById('table').innerHTML=str2
+
+str3='<table border="1">'
+for(let i=9;i>0;--i){
+    str2+='<tr>'
+    for(var j=i;j>0;--j){
+        str3+='<td>'+j+'*'+i+'='+(j*i)+'</td>'
+    }
+    str3+='</tr>'
+}
+str3+='</table>'
+document.getElementById('table3').innerHTML=str3
+
